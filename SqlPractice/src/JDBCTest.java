9899ea037d491997c2ee1435932d499781cbc61b
Akanksha <akankshaom131999@gmail.com>
2022-08-02 12:26:32 +0530

    ch

--- SqlPractice/src/JDBCTest.java
+++ SqlPractice/src/JDBCTest.java
@@ -7,77 +7,66 @@ import java.sql.Statement;
 public class JDBCTest {
 
 	public static void main(String[] args) {
-		insertdb();
+//		insertdb();
 //		readdb();
 //		updatedb();
+		deletedb();
 		
+		
+	}
+
+	private static void deletedb() {
+		// TODO Auto-generated method stub
+		//Connection connection = null;
+		//Statement statement = null;
+		
+		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "1234");
+				Statement statement = connection.createStatement()){
+			statement.execute("delete from account where idaccount=2");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+
 		
 	}
 
 	private static void insertdb() {
 		// TODO Auto-generated method stub
-		Connection connection = null;
-		Statement statement = null;
 		
-		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "1234");
-			statement = connection.createStatement();
+		
+		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "1234");
+				Statement statement = connection.createStatement()){
 			statement.execute("insert into account values(2,'aki','cbt',5)");
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally  {
-			try {
-				statement.close();
-				connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		} 
 		
 	}
 	private static void updatedb() {
-		// TODO Auto-generated method stub
-		Connection connection = null;
-		Statement statement = null;
+
 		
-		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "1234");
-			statement = connection.createStatement();
+		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "1234");
+				Statement statement = connection.createStatement()){
 			statement.execute("update account set amount=200 where idaccount=1 ");
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally  {
-			try {
-				statement.close();
-				connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		} 
 		
 	}
 
 	private static void readdb() {
 		// TODO Auto-generated method stub
-		Connection connection = null;
-		Statement statement = null;
-		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "1234");
-			statement = connection.createStatement();
+
+		
+		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "1234");
+				Statement statement = connection.createStatement()){
 			ResultSet resultSet = statement.executeQuery("select * from account");
 			while (resultSet.next()) {				
 				System.out.println(resultSet.getInt(1) + ", " + resultSet.getString(2) + ", " + resultSet.getString(3) + ", " + resultSet.getInt(4));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally  {
-			try {
-				statement.close();
-				connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		} 
 		
 	}
 	
